d42ac8e51e021fae64773b407dd52255817e0d63
deansrebel <alvayana01@gmail.com>
2024-08-01 19:20:52 +0300

    new lesson

--- Assets/Scripts/MagicNumbers.cs
+++ Assets/Scripts/MagicNumbers.cs
@@ -2,27 +2,33 @@ using UnityEngine;
 
 public class MagicNumbers : MonoBehaviour
 {
+    #region Variables
+
     // private int _max = 10000; // public не приветсвуется, но так можно изменять числа в Юнити
-   // private int _min = 500;
+    // private int _min = 500;
 
     [SerializeField] private int _max = 1000; // остаются приватными, но можно в юнити все ранво менять
     [SerializeField] private int _min = 500;
 
     private int _guess;
 
+    #endregion
+
+    #region Unity lifecycle
+
     private void Start()
     {
         Debug.Log($"Привет! Я Magic Numbers. Загадай число от {_min} до {_max}");
-       
+
         CalculateGuessAndLog();
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.DownArrow)) 
+        if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             //Debug.Log($"[{Time.frameCount}] GetKeyDown DownArrow");
-            
+
             _max = _guess;
             CalculateGuessAndLog();
         }
@@ -40,15 +46,21 @@ public class MagicNumbers : MonoBehaviour
             Debug.Log($"Ура! Твое число отгадано и равно {_guess}!");
         }
     }
+
+    #endregion
+
+    #region Private methods
+
     private void CalculateGuessAndLog()
     {
         // подсчитываю новый guess и вывожу log в консоль
         _guess = (_max + _min) / 2;
         Debug.Log($"Твое число равно: {_guess}?");
     }
+
+    #endregion
 }
 
 // bool isPressed = Input.GetKeyDown(KeyCode.DownArrow); // кнопка нажата. когда надо что-то сделать по нажатию
 // bool isPressed = Input.GetKeyUp(KeyCode.DownArrow); // кнопку отпустили. нужен сам момент отпусскания
 // //bool isPressed = Input.GetKey(KeyCode.DownArrow); // держим кнопку
-        
